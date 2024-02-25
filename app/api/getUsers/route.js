087ae9e6550b725530450e09001f1aa2ed642192
Sergio <principiante.en.programar@gmail.com>
2024-02-25 14:58:52 -0300

    revalidate for prisma

--- app/api/getUsers/route.js
+++ app/api/getUsers/route.js
@@ -1,6 +1,7 @@
 const { PrismaClient } = require('@prisma/client')
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
+export const revalidate = 0;
 
 async function main() {
     // ... you will write your Prisma Client queries here
